d20212bf845b8a19d61ea279a6a2e0065029f659
jdv <julien@crowdsec.net>
2024-09-07 16:38:34 +0200

    fix prinf numeric error, added some comments and quality of life, added top 10 attackers added README

--- crowdsec-efficiency-tester.sh
+++ crowdsec-efficiency-tester.sh
@@ -9,28 +9,48 @@ echo "
 
 show_end() {
   echo "
-If you have any questions or need help, please visit https://docs.crowdsec.net/u/troubleshooting/intro/ or join our community on https://crowdsec.net/community/
+Typical efficiency observed for the CrowdSec Intelligence Blocklist is 20 to 50%.
+If you have less than 10% efficiency, you may want to check the following:
+* The IPs in your logs are from ingress on an exposed service (website, api, ssh, etc.)
+* The IPs in your logs are not from a CDN but properly x-forwarded-for
+
+If you want to check what CrowdSec knows about an attacker IP visit https://app.crowdsec.net/cti/
+
+If you have any questions about our blocklists API, please visit https://doc.crowdsec.net/u/service_api/getting_started or join our community on https://crowdsec.net/community/
 "
 }
 
-# Default value for max lines
+# Max lines to process from the log file (here for performance reasons - Change it at your convenience)
 MAX_LINES=100000
+TOP_ATTACKERS_DISPLAY=10
 
 # Check required arguments
 if [ -z "$LOG_FILE" ] || [ -z "$API_KEY" ]; then
   echo "Usage: LOG_FILE=/path/to/log/file.log API_KEY=your-api-key ./crowdsec-efficiency-tester.sh"
 fi
 
-# Validate token is not empty
+# Validate API Key has been provided
+if [ -z "$API_KEY" ]; then
+  read -p "Enter your API key: " API_KEY
+fi
 if [ -z "$API_KEY" ]; then
   echo "Error: Api Key required"
   exit 1
 fi
 
+# Validate LOG_FILE has been provided
+if [ -z "$LOG_FILE" ]; then
+  read -p "Path to your log file: " LOG_FILE
+fi
+if [ -z "$LOG_FILE" ]; then
+  echo "Error: log file is required"
+  exit 1
+fi
+
 ### Step 1: Extract and count IPs from the log file
 echo -n "Extracting and counting IP addresses from logs..."
-IPS_FILE="ips-from-logs.txt"
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n $MAX_LINES | awk '{print $2","$1}' > "$IPS_FILE"
+PARSED_IPS_FILE="ips-from-logs.txt"
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n $MAX_LINES | awk '{print $2","$1}' > "$PARSED_IPS_FILE"
 echo " ✅"
 
 ### Step 2: Download blocklist
@@ -41,38 +61,54 @@ BLOCKLIST_CONTENT=$(curl -X 'GET' -s \
   -H "x-api-key: $API_KEY")
 echo " ✅"
 
-### Step 3: Analyze each IP against the blocklist
+### Step 3: Analyzing parsed IPs against the blocklist
 
-# Initialize counters
-TOTAL_REQUESTS=0
-BLOCKLIST_HIT_COUNT=0
-BLOCKLIST_HIT_REQUESTS=0
-TOTAL_IPS=0
-
-# Loop through each IP in the file
+# Initialize counters for report
+TOTAL_DISTINCT_IPS_IN_LOGS=0
+TOTAL_REQUESTS_IN_LOGS=0
+BLOCKLIST_IP_HITS=0
+BLOCKLIST_REQUESTS_HITS=0
+BAD_IPS=""
+# Count blocklist hits
 echo -n "Analyzing IPs against the blocklist..."
 while IFS=, read -r ip count; do
-  TOTAL_REQUESTS=$((TOTAL_REQUESTS + count))
-  TOTAL_IPS=$((TOTAL_IPS + 1))
+  TOTAL_REQUESTS_IN_LOGS=$((TOTAL_REQUESTS_IN_LOGS + count))
+  TOTAL_DISTINCT_IPS_IN_LOGS=$((TOTAL_DISTINCT_IPS_IN_LOGS + 1))
   if echo "$BLOCKLIST_CONTENT" | grep -qx "$ip"; then
-    BLOCKLIST_HIT_COUNT=$((BLOCKLIST_HIT_COUNT + 1))
-    BLOCKLIST_HIT_REQUESTS=$((BLOCKLIST_HIT_REQUESTS + count))
+    BLOCKLIST_IP_HITS=$((BLOCKLIST_IP_HITS + 1))
+    BLOCKLIST_REQUESTS_HITS=$((BLOCKLIST_REQUESTS_HITS + count))
+
+     # Concatenate the IP and count to the HIT_IPS string
+    BAD_IPS="${BAD_IPS}${ip}, ${count}\n"
   fi
-done < "$IPS_FILE"
+done < "$PARSED_IPS_FILE"
 
 # Remove the temporary IPs file
-rm "$IPS_FILE"
+rm "$PARSED_IPS_FILE"
 echo " ✅"
 
 ### Step 4: Efficiency calculations
-IP_EFFICIENCY=$(bc <<< "scale=4; $BLOCKLIST_HIT_COUNT / $TOTAL_IPS * 100")
-REQUEST_EFFICIENCY=$(bc <<< "scale=4; $BLOCKLIST_HIT_REQUESTS / $TOTAL_REQUESTS * 100")
+LC_NUMERIC=C
+IP_EFFICIENCY=$(bc <<< "scale=4; $BLOCKLIST_IP_HITS / $TOTAL_DISTINCT_IPS_IN_LOGS * 100")
+REQUEST_EFFICIENCY=$(bc <<< "scale=4; $BLOCKLIST_REQUESTS_HITS / $TOTAL_REQUESTS_IN_LOGS * 100")
 FORMATTED_IP_EFFICIENCY=$(printf "%.2f" $IP_EFFICIENCY)
 FORMATTED_REQUEST_EFFICIENCY=$(printf "%.2f" $REQUEST_EFFICIENCY)
 
 ### Step 5: Display summary
+# Hits ratio
 echo "
-Blocklist hits (IPs)       : $FORMATTED_IP_EFFICIENCY% ($BLOCKLIST_HIT_COUNT/$TOTAL_IPS)
-Blocklist hits (Requests)  : $FORMATTED_REQUEST_EFFICIENCY% ($BLOCKLIST_HIT_REQUESTS/$TOTAL_REQUESTS)"
+
+=== Summary ===
+Blocklist hits (IPs)       : $FORMATTED_IP_EFFICIENCY% ($BLOCKLIST_IP_HITS/$TOTAL_DISTINCT_IPS_IN_LOGS)
+Blocklist hits (Requests)  : $FORMATTED_REQUEST_EFFICIENCY% ($BLOCKLIST_REQUESTS_HITS/$TOTAL_REQUESTS_IN_LOGS)"
+
+# Top 10 Attackers IPs
+echo "
+TOP $TOP_ATTACKERS_DISPLAY IPs in the blocklist:
+------------------------------------------------"
+
+printf "%-20s | %-5s\n" "IP Address" "Count"
+printf "%-20s | %-5s\n" "--------------------" "-----"
+echo -e $BAD_IPS | sort -t',' -k2,2nr | head -n$TOP_ATTACKERS_DISPLAY | awk -F',' '{ printf "%-20s | %-5s\n", $1, $2 }'
 
 show_end
